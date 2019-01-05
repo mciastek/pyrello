340ed3a7bd57d66808c8b9e37a26dd7608125aa0
Mirosław Ciastek <mirek@swingdev.io>
2019-01-05 16:47:38 +0100

    Increase workers

--- boot.sh
+++ boot.sh
@@ -2,4 +2,4 @@
 source venv/bin/activate
 # flask db upgrade
 # flask translate compile
-exec gunicorn -b :5000 --reload --access-logfile - --error-logfile - pyrello:app
+exec gunicorn -w 2 -b :5000 --reload --access-logfile - --error-logfile - pyrello:app
